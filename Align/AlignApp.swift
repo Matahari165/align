78e80f40bc6d8734f5b4320aa5032c36861f3287
Matahari165 <307921095+Matahari165@users.noreply.github.com>
2026-08-28 19:56:59 +0200

    Initial Commit

---
+++ Align/AlignApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlignApp.swift
+//  Align
+//
+//  Created by Jérémy Delloume on 28/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct AlignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
